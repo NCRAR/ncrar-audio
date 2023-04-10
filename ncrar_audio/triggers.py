b7d50bfa5a0bde6c9976787f02db4b3cc9415605
Brad Buran <info@bradburan.com>
2023-04-10 15:45:51 -0700

    Move to generic make_trigger function

--- ncrar_audio/triggers.py
+++ ncrar_audio/triggers.py
@@ -1,18 +1,41 @@
 import numpy as np
 
 
-def make_analog_trigger(fs, n, duration=0.1):
-    n_samp = int(round(duration * fs))
-    trigger = np.zeros(n)
-    trigger[:n_samp] = 0.5
-    return trigger
+def _square_waveform(fs, duration=0.01):
+    return np.ones(int(round(fs * duration)))
 
 
-def make_analog_trigger_cos(fs, n, pulse_frequency=128, pulse_cycles=1):
+def _cos_waveform(fs, pulse_frequency=128, pulse_cycles=1, repeat=1):
     n_samp = int(round((1 / pulse_frequency) * fs)) * pulse_cycles
     t = np.arange(n_samp) / fs
-    pulse = np.sin(2 * np.pi * pulse_frequency * t)
+    y = np.sin(2 * np.pi * pulse_frequency * t)
+    y = np.tile(y, (repeat, 1))
+    y[1:] *= 0.5
+    return y.ravel()
 
-    trigger = np.zeros(n, dtype='double')
-    trigger[:n_samp] = pulse
-    return trigger
+
+def make_trigger(fs, n, repeat=1, iti=0.02, shape='square',
+                 shape_settings=None):
+
+    n_trig = int(round(fs * iti))
+    trig = np.zeros(n_trig)
+    if shape_settings is None:
+        shape_settings = {}
+    fn = globals()[f'_{shape}_waveform']
+    w = fn(fs, **shape_settings)
+    if len(w) > n_trig:
+        raise ValueError('ITI too short')
+    trig[:len(w)] = w
+
+    trig = np.tile(trig, (repeat, 1))
+    trig[1:] *= 0.5
+    trig = trig.ravel()
+
+    if len(trig) > n:
+        raise ValueError('Trigger sequence is too long')
+
+    waveform = np.zeros(n)
+    waveform[:len(trig)] = trig
+    return waveform
+
+
