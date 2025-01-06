c8c8ad529228a8999cdc0bf6aae6412851f6ec16
Brad Buran <buran@ohsu.edu>
2025-01-06 13:54:43 -0800

    Tweak acquire_waveform example

--- examples/acquire_waveform.py
+++ examples/acquire_waveform.py
@@ -14,13 +14,14 @@ from ncrar_audio.babyface import Babyface
 device = Babyface('XLR1', 'XLR2', use_osc=False)
 cal = FlatCalibration.unity()
 waveform = tone(fs=device.fs, duration=1, frequency=1e3, level=-20, calibration=cal)
-recording = device.acquire(waveform, 2)
+recording = device.acquire(waveform, 1)
 t = np.arange(5000) / device.fs
-plt.plot(t, recording[..., :5000].T)
+plt.plot(t, recording[..., :5000].T, label='Recording')
 
 plt.axvline(0.046439909297052155, c='k', label='Sound driver delay')
 plt.axvline(0.046439909297052155 + 12 / device.fs, ls=':', c='k',
             label='Sound driver delay plus DA/AD delay')
 plt.xlabel('Time (sec)')
 plt.ylabel('Amplitude (Volts)')
+plt.legend()
 plt.show()
